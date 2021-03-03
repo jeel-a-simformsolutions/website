4c47ddb0cbb82f5447d632fcdc77288cda16ab62
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-03-03 10:10:30 +0530

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 import Home from './Home';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import About from './About';
+import Service from './Service';
+import Contect from './Contect';
+import Navbar from './Navbar';
 import { Switch } from "react-router-dom";
 
 const App = () => {
     return (
         <>
-        <Switch>
-        <Route/>
-        <Home/>
+        <Navbar/>
+        <Switch >
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/contect" component={Contect}/>
+        <Route exact path="/service" component={Service}/>
+        <Redirect to="/" />
         </Switch>
           
         </>
---
+++ src/Contect.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contect = () => {
+    return (
+        <>
+        <h1>Welcome to the contect page</h1>
+        </>
+    )
+}
+
+export default Contect;
